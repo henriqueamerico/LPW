deb9a5289a82acd52d804ac218ccfc453ef9b173
Henrique Américo <henriquelopesamerico@gmail.com>
2026-03-11 09:58:32 -0300

    Add files via upload

---
+++ Henrique/aula3/exemplo_post.php
@@ -0,0 +1,14 @@
+<?php
+
+echo "Parâmetros recebidos por POST: ";
+
+echo "<br>";
+echo $_POST['nome'];
+
+echo "<br>";
+$idade = $_POST['idade'];
+echo "idade da pessoa:" . $idade;
+
+
+
+//http://localhost/LPW-main/Henrique/aula3/exemplopost.php
