68ce27418de7835444f0894754c4aec459e2140b
Henrique Américo <henriquelopesamerico@gmail.com>
2026-03-18 09:59:19 -0300

    Add files via upload

---
+++ Henrique/aula3/modelo/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Escolha a pessoa</title>
+</head>
+<body>
+
+    <span style="font-weight: bold;">Henrique</span>
+    <a href="pagina.php?tipo=C&nome=Henrique&sobrenome=Américo&idade=17">
+
+    Clique aqui</a>
+    
+</body>
+</html>
