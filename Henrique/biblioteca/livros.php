921d720a6dfd90d1cce9423aca0ad6cda8f7d884
Henrique Américo <henriquelopesamerico@gmail.com>
2026-05-13 09:57:39 -0300

    Update livros.php

--- Henrique/biblioteca/livros.php
+++ Henrique/biblioteca/livros.php
@@ -4,18 +4,26 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-require_once ("util/Conexao.php");
+require_once("util/Conexao.php");
 
 $conexao = Conexao::getConexao();
 //print_r($conexao);
-
-// Salvar o livro
-if(isset($_POST['titulo'])) {
+if (isset($_POST['titulo'])) {
     // 1- receber os dados do formulário
-    $titulo = $_POST['titulo'];
-    $genero = $_POST['genero'];
+    $titulo = trim($_POST['titulo']) ? trim($_POST['titulo']) : null;
+    $genero = trim($_POST['genero']) ? trim($_POST['genero']) : null;
+    $qtdPag = is_numeric($_POST['paginas']) ? $_POST['paginas'] : null;
+    $autor = trim($_POST['autor']) ? trim($_POST['autor']) : null;
+
+
+
+
     $qtdPag = $_POST['paginas'];
     $autor = $_POST['autor'];
+
+    // 1.1 - Validar os dados
+
+
 
     //echo $titulo . " - " . $genero . " - " . $qtdPag;
 
@@ -27,7 +35,6 @@ if(isset($_POST['titulo'])) {
 
     // 3- Redirecionar para a página de listagem
     header("Location: livros.php");
-
 }
 
 // Listagem dos livros
@@ -41,11 +48,13 @@ $livros = $stmt->fetchAll();
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastro de livros</title>
 </head>
+
 <body>
 
     <h1>Cadastro de livros</h1>
@@ -54,81 +63,88 @@ $livros = $stmt->fetchAll();
 
     <table border="1">
         <!-- cabeçalho -->
-         <tr>
+        <tr>
             <th>ID</th>
             <th>Título</th>
             <th>Gênero</th>
             <th>Páginas</th>
             <th>Autor</th>
-         </tr>
+        </tr>
 
-         <!-- Dados -->
-          <?php foreach($livros as $l): ?>
+        <!-- Dados -->
+        <?php foreach ($livros as $l): ?>
             <tr>
-                <td><?= $l["id"] ?></td>  
+                <td><?= $l["id"] ?></td>
                 <td><?= $l["titulo"] ?></td>
-                
-                <td>
-                   <?php
-                   switch ($l["genero"]) {
-                    case "D":
-                        echo "Drama";
-                        break;
-                    case "F":
-                        echo "Ficção";
-                        break;
-                    case "R":
-                        echo "Romance";
-                        break;
 
-                    default:
-                        echo "Outro";
-                   }
-                   ?>
-                </td>  
-                <td><?= $l["qtd_paginas"] ?></td> 
-                <td><?= $l["autor"] ?></td> 
-            
-            <td>
-                <a href="livros_excluir.php?id=<?= $l['id'] ?>"
-                        onclick="if (! confirm('Você jura pela vida da sua mãe que vc quer excluir?')) return false"
-                    >Excluir</a>
-            </td>
+                <td>
+                    <?php
+                    switch ($l["genero"]) {
+                        case "D":
+                            echo "Drama";
+                            break;
+                        case "F":
+                            echo "Ficção";
+                            break;
+                        case "R":
+                            echo "Romance";
+                            break;
+
+                        default:
+                            echo "Outro";
+                    }
+                    ?>
+                </td>
+                <td><?= $l["qtd_paginas"] ?></td>
+                <td><?= $l["autor"] ?></td>
+
+                <td>
+                    <a href="livros_excluir.php?id=<?= $l['id'] ?>"
+                        onclick="if (! confirm('Você jura pela vida da sua mãe que vc quer excluir?')) return false">Excluir</a>
+                </td>
             </tr>
         <?php endforeach; ?>
 
     </table>
 
     <h3>Formulário</h3>
+    <!-- <form action="" method="POST" onsubmit="return validarForm();"> -->
     <form action="" method="POST">
 
-    <input type="text" placeholder="Informe o título"
-    name="titulo">
+        <input type="text" placeholder="Informe o título"
+            name="titulo" id="titulo">
 
-    <br><br>
+        <br><br>
 
-    <select name="genero">
-        <option value="">---Selecione o gênero---</option>
-        <option value="D">Drama</option>
-        <option value="F">Ficção</option>
-        <option value="R">Romance</option>
-    </select>
+        <select name="genero" id="genero">
+            <option value="">---Selecione o gênero---</option>
+            <option value="D">Drama</option>
+            <option value="F">Ficção</option>
+            <option value="R">Romance</option>
+        </select>
 
-    <br><br>
+        <br><br>
 
-    <input type="number" name= "paginas"
-    placeholder="Informe o número de páginas">
+        <input type="number" name="paginas" id="paginas"
+            placeholder="Informe o número de páginas">
 
-    <br><br>
+        <br><br>
 
-    <input type="text" placeholder="Informe o Autor"
-    name="autor">
+        <input type="text" placeholder="Informe o Autor"
+            name="autor" id="autor">
 
-    <br><br>
+        <br><br>
 
-    <button>Enviar</button>
+        <button>Enviar</button>
 
     </form>
-    
+
+    <div id="msgErro" style="color: red; display: none;">
+        Exemplo de erro!
+    </div>
+
+    <script src="validacao.js"></script>
+
 </body>
+
 </html>
